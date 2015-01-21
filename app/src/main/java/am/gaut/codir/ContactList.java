889d55b8f9e20275900bf632a4df31f4624a953e
Gautam Gupta <gautam@gaut.am>
2015-01-20 19:29:52 -0500

    Introduce AsyncHttpClient. Do basic fetch of full name, cell, thumbnail. Todo: Better layout, single contact activity.

---
+++ app/src/main/java/am/gaut/codir/ContactList.java
@@ -0,0 +1,78 @@
+package am.gaut.codir;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+
+public class ContactList extends ArrayAdapter<String> {
+    public static final String TAG = "CoDirMainCL";
+
+    private final Activity mContext;
+
+    private final String[] fnames;
+    private final String[] lnames;
+    private final String[] cells;
+    private final String[] thumbs;
+
+    public ContactList(Activity mContext, String[] fnames, String[] lnames, String[] cells, String[] thumbs) {
+        super(mContext, R.layout.contact_list_row, fnames);
+
+        this.mContext = mContext;
+
+        this.fnames = fnames;
+        this.lnames = lnames;
+        this.cells  = cells;
+        this.thumbs = thumbs;
+    }
+
+    @Override
+    public View getView(int p, View view, ViewGroup parent) {
+        LayoutInflater inflater = mContext.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.contact_list_row, null, true);
+
+        TextView txtName = (TextView) rowView.findViewById(R.id.txtName);
+        TextView txtCell = (TextView) rowView.findViewById(R.id.txtCell);
+        ImageView imgThumb = (ImageView) rowView.findViewById(R.id.imgThumb);
+
+        txtName.setText(fnames[p] + " " + lnames[p]);
+        txtCell.setText(cells[p]);
+        new DownloadImageTask((ImageView) rowView.findViewById(R.id.imgThumb)).execute(thumbs[p]);
+
+        return rowView;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView mThumb;
+
+        public DownloadImageTask(ImageView mThumb) {
+            this.mThumb = mThumb;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            mThumb.setImageBitmap(result);
+        }
+    }
+}
--- app/src/main/java/am/gaut/codir/MainActivity.java
+++ app/src/main/java/am/gaut/codir/MainActivity.java
@@ -2,21 +2,95 @@ package am.gaut.codir;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = "CoDirMain";
 
-    private ListView mContactList;
+    public ListView mContactList;
+    String[] seeds;
+    public AsyncHttpClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContactList = (ListView) findViewById(R.id.contact_list);
+
+        client = new AsyncHttpClient();
+        client.get("http://api.randomuser.me/?results=100", new JsonHttpResponseHandler() {
+            ContactList adapter;
+
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+                JSONArray results = null;
+                try {
+                    results = response.getJSONArray("results");
+
+                    Log.i(TAG, "Fetching contacts");
+
+                    seeds           = new String[results.length()]; // Basically IDs
+                    String[] fnames = new String[results.length()];
+                    String[] lnames = new String[results.length()];
+                    String[] cells  = new String[results.length()];
+                    String[] thumbs = new String[results.length()];
+
+                    for(int i = 0; i < results.length(); i++) {
+                        seeds[i]  = results.getJSONObject(i).getString("seed");
+                        fnames[i] = results.getJSONObject(i).getJSONObject("user").getJSONObject("name").getString("first");
+                        lnames[i] = results.getJSONObject(i).getJSONObject("user").getJSONObject("name").getString("last");
+                        cells[i]  = results.getJSONObject(i).getJSONObject("user").getString("cell");
+                        thumbs[i] = results.getJSONObject(i).getJSONObject("user").getJSONObject("picture").getString("thumbnail");
+                    }
+
+                    adapter = new ContactList(MainActivity.this, fnames, lnames, cells, thumbs);
+                    mContactList.setAdapter(adapter);
+                    mContactList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int p, long id) {
+                            final String seed = seeds[p];
+
+                            // Load Single Contact Activity
+                        }
+                    });
+
+                } catch (JSONException e) {
+                    Log.e(TAG, "Failed to parse JSON. " + e.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.e(TAG, "Failed to get contacts.");
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+                Log.e(TAG, "Retrying #" + retryNo);
+            }
+        });
     }
 
 
